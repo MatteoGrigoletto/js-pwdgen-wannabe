b9f65a7dd9b1b678ca2d4b1af6590d9bd953e1a0
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-19 14:53:56 +0200

    first commit

---
+++ js/password.js
@@ -0,0 +1,13 @@
+// nome utente
+const nome= prompt('inserisci nome utente');
+// cognome utente
+const cognome= prompt('inserisci cognome utente');
+//colore utente
+let colore= prompt('inserisci colore utente');
+//genero password
+const userPassword = `La tua password e'
+ ${nome} 
+ ${cognome}
+ ${colore} 76 `;
+//stampo password su file html
+document.querySelector('p').innerHTML = userPassword;
